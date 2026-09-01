c4a3c58c9576c97880e479a1ede98eaf8b069206
Mani Ramezan <mani.ramezan@gmail.com>
2026-09-01 00:44:23 -0400

    Add reusable ActionPill component (#90)
    * Add reusable action pill component
    
    * Use domain-neutral action pill examples

---
+++ components/src/main/kotlin/io/github/maniramezan/compose/components/ActionPillPreview.kt
@@ -0,0 +1,22 @@
+package io.github.maniramezan.compose.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.android.showkase.annotation.ShowkaseComposable
+import io.github.maniramezan.compose.theme.AppTheme
+import io.github.maniramezan.compose.utils.PreviewFontScale
+import io.github.maniramezan.compose.utils.PreviewLightDark
+
+@PreviewLightDark
+@PreviewFontScale
+@Preview(name = "Action Pill", group = "Actions")
+@Composable
+public fun ActionPillPreview(): Unit =
+    AppTheme {
+        ActionPill(onClick = {}) { Text(text = "View details") }
+    }
+
+@ShowkaseComposable(name = "Action Pill", group = "Actions")
+@Composable
+public fun ActionPillShowkase(): Unit = ActionPillPreview()
